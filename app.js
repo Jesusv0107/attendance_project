7cc65a953f4a4c233904ce9d51204719dee67750
jesusv0107 <jesus.velasco@rdpolytech.ca>
2024-08-19 11:32:54 -0600

    firstcommit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+//connect to mongoose
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {console.log(`Connected to MongoDB Database`)})
+.catch((err) => {console.log(`Error Connected to MongoDB Database: ${err}`)});
+
+//view engines
+app.set('view engine', 'ejs');
+app.set('views' , './views');
+
+
+
+//Middelewares
+app.use(express.static('public'));
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Connected to port ${PORT}`);
+
+});
