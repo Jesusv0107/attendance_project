b3afc6b4b150ea7d572a7d5f969b4365ae00defa
jesusv0107 <jesus.velasco@rdpolytech.ca>
2024-08-24 23:26:47 -0600

    firstcommit

--- app.js
+++ app.js
@@ -2,29 +2,48 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
+const authRoutes = require('./routes/authRoutes.js');
+const studentRoutes = require('./routes/studentRoutes.js');
 const PORT = process.env.PORT || 3000;
 
-//connect to mongoose
+//Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {console.log(`Connected to MongoDB Database`)})
-.catch((err) => {console.log(`Error Connected to MongoDB Database: ${err}`)});
+.then(() => { console.log(`Connected to MongoDB Database`) })
+.catch((err) => { console.log(`Error connecting to database: ${err}`) });
 
-//view engines
+//View Engines
 app.set('view engine', 'ejs');
-app.set('views' , './views');
+app.set('views', './views');
 
-
-//Middelewares
+//Middlewares
 app.use(express.static('public'));
+app.use('/', authRoutes);
+app.use('/', studentRoutes);
 
-app.use((err, res , req, next) => {
+app.use((err, res, req, next) =>{
     console.error(err.stack);
-    res.status(500).send('Internal Server Error');
+    res.status.send('Internal Server Error');
     next();
 });
 
-//start server
-app.listen(PORT, () => {
+//Start server
+app.listen(PORT, () =>{
     console.log(`Connected to port ${PORT}`);
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
