2fa09650920626d4d8a77b4e4563f94229169c52
jesusv0107 <jesus.velasco@rdpolytech.ca>
2024-08-26 13:25:45 -0600

    first

--- models/attendanceManager.js
+++ models/attendanceManager.js
@@ -3,26 +3,22 @@ const mongoose = require('mongoose');
 
 //Defining the schema for the AttendanceManager
 const attendanceManagerSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        default: 'Attendance Manager',
-        enum: ['attendance-manager', 'Admin'],
-
-    },
-    
-    //timestamps: true,
-
-
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: 'Attendance Manager',
+        enum: ['Attendance Manager', 'Admin'],
+    },
+  //  timestamps: true,
 });
 
 const AttendanceManager = mongoose.model('AttendanceManager', attendanceManagerSchema);
